0a7b02dddb49d17cd7703bbe3e6368ff6f9b2830
Dementiev Nikolay <dementievnikolay@gmail.com>
2017-09-21 12:41:40 +0300

    add notes' screen view

---
+++ SuperDealTest/NotesList/Controller/MainNoteViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainNoteViewController.swift
+//  SuperDealTest
+//
+//  Created by Nikolay Dementiev on 21.09.17.
+//  Copyright © 2017 mc373. All rights reserved.
+//
+
+import UIKit
+
+class MainNoteViewController: UIViewController {
+
+    @IBOutlet weak var notesTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
